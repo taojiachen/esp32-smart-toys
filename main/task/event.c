c2e6f93885d98abc177b755d4f857a12321fefb4
taojiachen <18809760475@163.com>
2024-11-10 21:29:19 +0800

    修改了一些问题

--- main/task/event.c
+++
@@ -1,9 +0,0 @@
-#include "esp_log.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#include "event_source.h"
-#include "esp_event_base.h"
-
-static const char* TAG = "user_event_loops";
